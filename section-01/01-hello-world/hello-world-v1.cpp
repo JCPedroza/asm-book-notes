ff8c8ff2abcf223972b148a1d43d233436fac9a9
JCPedroza <nemesis4go10@hotmail.com>
2023-03-27 21:56:33 -0600

    note: add more notes

--- section-01/01-hello-world/hello-world-v1.cpp
+++ section-01/01-hello-world/hello-world-v1.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main(int argc, char * argv[]) { // Alternative: char ** argv
     // Iterate until argv is a null pointer
+    // We can treat argv as a pointer because array arguments are demoted to 
+    // pointers.
     while (*argv) {
         cout << *(argv++) << endl;
     }
